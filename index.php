54b3055fae9111d411c277380955a155635bc10a
maximiliano1401 <alu.2411155@itesrenedescartes.edu.mx>
2024-11-11 19:46:05 -0600

    modificion

--- index.php
+++ index.php
@@ -48,22 +48,6 @@ session_start();
         <div class="category">Controles y accesorios</div>
         <div class="category">Videojuegos</div>
     </section>
-
-    <!-- <section class="products"> -->
-        <!-- Each product item -->
-        <!-- <div class="product"> -->
-            <!-- <img src="img/cod.jpg" alt="Call of Duty: Black Ops 6 - PS5"> -->
-            <!-- <p>Call of Duty: Black Ops 6 - PS5 - PlayStation 5</p> -->
-            <!-- <span class="price">$1,439.00</span> <span class="original-price">$1,599.00</span> -->
-        <!-- </div> -->
-        <!-- <div class="product"> -->
-            <!-- <img src="img/mario-party.jpg" alt="Super Mario Party - Nintendo Switch"> -->
-            <!-- <p>Super Mario Party - Nintendo Switch</p> -->
-            <!-- <span class="price">$1,599.00</span> -->
-        <!-- </div> -->
-        <!-- Add more products as needed following this format -->
-    <!-- </section> -->
-    
     <?php
 
     include "procesos/mostrar_productos.php";
