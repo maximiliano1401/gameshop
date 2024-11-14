ee797a4e0a5f3a395ed413594b7acf6894edfcc2
PABLO_SALOMON\salom <salomonpablo649@gmail.com>
2024-11-13 18:18:02 -0600

    perfil usuario vista y css

---
+++ perfil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil - Game Shop</title>
+    <link rel="stylesheet" href="Css/perfil.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            Game Shop - Perfil
+        </div>
+        <div class="profile-info">
+            <img src="https://via.placeholder.com/140" alt="Perfil">
+            
+            <button class="button">
+                <span class="title">Tu información y seguridad</span>
+                <span class="description">Nombre, datos y contraseña para identificarte</span>
+            </button>
+            <button class="button">
+                <span class="title">Tarjetas</span>
+                <span class="description">Información de tarjetas</span>
+            </button>
+            <button class="button">
+                <span class="title">Dirección</span>
+                <span class="description">Direcciones</span>
+            </button>
+        </div>
+    </div>
+</body>
+</html>
