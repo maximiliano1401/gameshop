41fb6fa34834dffbb97c237b4264ab8e46d82f5c
maximiliano1401 <alu.2411155@itesrenedescartes.edu.mx>
2024-11-25 17:24:56 -0600

    css

--- perfil.php
+++ perfil.php
@@ -17,6 +17,7 @@ if (!isset($_SESSION["UsuarioID"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil - Game Shop</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="Css/perfil.css">
     <style>
         /* Modales */
