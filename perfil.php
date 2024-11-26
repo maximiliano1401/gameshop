ebcf19646648e1b2b011a4a4c8ec836ba6a55901
maximiliano1401 <alu.2411155@itesrenedescartes.edu.mx>
2024-11-26 17:11:35 -0600

    act

--- perfil.php
+++ perfil.php
@@ -237,9 +237,9 @@ if (!isset($_SESSION["UsuarioID"])) {
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="modalLabel">Mensaje</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
-                    </button>
+                    </button> -->
                 </div>
                 <div class="modal-body" id="modal-message">
                     <!-- El mensaje se mostrará aquí -->
